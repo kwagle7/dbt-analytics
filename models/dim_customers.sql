b41fc891ccb6854fc8bcb49a500779bdcf1bddbe
Kshitij Wagle <kshitij.wagle1@gmail.com>
2024-02-26 05:38:10 +0000

    added materialized table

--- models/dim_customers.sql
+++ models/dim_customers.sql
@@ -1,18 +1,34 @@
-with customers as (
+{{
+    config(
+        materialized ='table'
+    )
+}}
 
-select * from {{ ref('stg_customers')}}
+
+with customers as (
+    select * from {{ref ('stg_customers')}}
+
+    -- select
+    --     id as customer_id,
+    --     first_name,
+    --     last_name
+
+    -- from `dbt-tutorial`.jaffle_shop.customers
 
 ),
 
 orders as (
+        
+        select * from {{ref ('stg_orders')}}
 
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
 
-    from `dbt-tutorial`.jaffle_shop.orders
+    -- select
+    --     id as order_id,
+    --     user_id as customer_id,
+    --     order_date,
+    --     status
+
+    -- from `dbt-tutorial`.jaffle_shop.orders
 
 ),
 
